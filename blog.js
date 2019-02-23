f45d34f1de3383a3d4f34ee2cce5d6ca403807f3
Ginny <happyhary@gmail.com>
2019-02-23 14:43:20 +0100

    post

--- blog.js
+++ blog.js
@@ -7,6 +7,13 @@ let a = $('input.gomb').on('click', (event) => {
   let posting = $('textarea').val();
   $('div.posztok').append(`<p>${posting}<p/>`);
   let dataToSave = { post: post, posting: posting };
-  console.log("hello")
   database.ref("brightyblog").push(dataToSave);
 })
+
+database.ref("brightyblog").once('value').then(data => {
+  data.forEach(element => {
+    console.log(element.child("post").val())
+    $('div.posztok').append(`<h1>${element.child("post").val()}</h1>`);
+    $('div.posztok').append(`<p>${element.child("posting").val()}</p>`);
+  })
+});
