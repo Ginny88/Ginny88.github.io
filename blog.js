ce5be6a1b66a702b5e9a712f1eb2ede33325bb5f
Ginny <happyhary@gmail.com>
2019-02-23 14:34:50 +0100

    push

--- blog.js
+++ blog.js
@@ -1,11 +1,12 @@
-console.log("no");
+
 let a = $('input.gomb').on('click', (event) => {
   event.preventDefault();
-  console.log("asdasd");
+
   let post = $('input[type="text"]').val();
   $('div.posztok').append(`<p>${post}</p>`);
   let posting = $('textarea').val();
   $('div.posztok').append(`<p>${posting}<p/>`);
-let dataToSave = {post: post , posting: posting};
-database.ref("post").push(dataToSave);  
+  let dataToSave = { post: post, posting: posting };
+  console.log("hello")
+  database.ref("brightyblog").push(dataToSave);
 })
